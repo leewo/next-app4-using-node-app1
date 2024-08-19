a870bb83d07a5deff5475f7486c5d67725cba61e
wy <xiph@live.com>
2024-08-19 20:45:09 +0900

    logout 변경으로 인한 수정 - useRouter 훅을 사용하여 로그아웃 후 메인 페이지로 이동 - 로그아웃 실패 시 사용자에게 알림을 표시

--- src/app/components/logoutbutton.tsx
+++ src/app/components/logoutbutton.tsx
@@ -1,32 +1,27 @@
 'use client';
 
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import { useAuth } from './authcontext';
 
 const LogoutButton: React.FC = () => {
   const { user, logout } = useAuth();
+  const router = useRouter();
 
   const handleLogout = async () => {
     if (window.confirm(`${user?.USER_NAME}님 로그아웃 하시겠습니까?`)) {
       try {
-        const response = await fetch("http://localhost:3001/api/v1/logout", {
-          method: "POST",
-          credentials: 'include'
-        });
-
-        if (response.ok) {
-          logout(); // AuthContext의 logout 함수 호출
-        } else {
-          console.error("Logout failed");
-        }
+        await logout();
+        router.push('/'); // 메인 페이지로 이동
       } catch (error) {
         console.error("Logout error:", error);
+        alert('로그아웃 중 오류가 발생했습니다. 다시 시도해 주세요.');
       }
     }
   };
 
   return (
-    <button
+    <button 
       onClick={handleLogout}
       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
     >
